c57cb91f7d99c4154bb8e4ce97bc5853450b9e3b
ddieruf <david@dierufventures.com>
2019-02-26 15:08:00 -0700

    Add dynamic logging

--- src/PalTracker/Program.cs
+++ src/PalTracker/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
 
 
 namespace PalTracker
@@ -22,6 +23,13 @@ namespace PalTracker
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(config => config.AddCloudFoundry())
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseCloudFoundryHosting()
+                .ConfigureLogging((builderContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddDynamicConsole();
+                    loggingBuilder.AddDebug();
+                });
     }
 }
