4c64f3d4b52d93ac14a69951e57e844b9153272a
ddieruf <david@dierufventures.com>
2019-02-26 11:47:08 -0700

    Replace in-mem db with mysql

---
+++ src/PalTracker/MySqlTimeEntryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalTracker
+{
+    public class MySqlTimeEntryRepository : ITimeEntryRepository
+    {
+        private TimeEntryContext _context;
+
+        public MySqlTimeEntryRepository(TimeEntryContext context)
+        {
+            this._context = context;
+        }
+
+        public bool Contains(long id)
+        {
+            return (FindRecord(id) != null);
+        }
+
+        public TimeEntry Create(TimeEntry timeEntry)
+        {
+            TimeEntryRecord record = _context.TimeEntryRecords.Add(timeEntry.ToRecord()).Entity;
+            _context.SaveChanges();
+            return record.ToEntity();
+        }
+
+        public void Delete(long id)
+        {
+            _context.TimeEntryRecords.Remove(FindRecord(id));
+            _context.SaveChanges();
+        }
+
+        public TimeEntry Find(long id)
+        {
+            return FindRecord(id).ToEntity();
+        }
+
+        public IEnumerable<TimeEntry> List()
+        {
+            return _context.TimeEntryRecords.ToList()
+                .Select(record => record.ToEntity());
+        }
+
+        public TimeEntry Update(long id, TimeEntry timeEntry)
+        {
+            TimeEntryRecord record = timeEntry.ToRecord();
+            record.Id = id;
+
+            _context.TimeEntryRecords.Update(record);
+            _context.SaveChanges();
+            return Find(id);
+        }
+
+        private TimeEntryRecord FindRecord(long id)
+        {
+            return _context.TimeEntryRecords.AsNoTracking().FirstOrDefault(q => q.Id == id);
+        }
+    }
+}
---
+++ src/PalTracker/TimeEntryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalTracker
+{
+    public class TimeEntryContext : DbContext
+    {
+        public TimeEntryContext(DbContextOptions dbContextOptions)
+            : base (dbContextOptions)
+        {
+        }
+
+        public DbSet<TimeEntryRecord> TimeEntryRecords { get; set; }
+    }
+}
