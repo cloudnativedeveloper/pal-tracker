fb0d719da219a3144d8014ff0c7cf6028df51bfb
ddieruf <david@dierufventures.com>
2019-02-26 09:13:36 -0700

    Fix build.sh database folder location

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ mkdir -p $artifacts_path
 
 cp manifest-*.yml $build_output
 cp scripts/migrate-databases.sh $build_output
-cp -r databases $build_output
+cp -r src/PalTracker/databases $build_output
 
 dotnet publish src/PalTracker --configuration Release \
     --output $build_output/src/PalTracker/bin/Release/netcoreapp2.1/publish
